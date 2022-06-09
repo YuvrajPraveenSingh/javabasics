8e58af56dd7c03138a974f3c35850cfe2892f6b7
Yuvraj Singh <yuv00191@gmail.com>
2022-06-09 09:00:01 +0530

    3rd commit

--- src/main.java
+++ src/main.java
@@ -26,7 +26,7 @@ class Factorial {
 
 
 
-    class ListOfNumbers {
+class ListOfNumbers {
 
         // create an integer array
         private int[] list = {5, 6, 8, 9, 2};
@@ -72,4 +72,64 @@ class Factorial {
             ListOfNumbers list = new ListOfNumbers();
             list.writeList();
         }
+}
+class  ReverseANumberUsingWhileLoop {
+
+    public static void main(String[] args ){
+     int Reverse = 0;
+
+     Scanner sc = new Scanner(System.in);
+     System.out.println("Enter the number : ");
+     int Number = sc.nextInt();
+     sc.close();
+
+     while (Number != 0){
+         int Remainder = Number % 10;
+         Reverse = Reverse*10 + Remainder;
+         Number = Number/10;
+     }
+     System.out.println("The Reverse of the Number is : " + Reverse);
     }
+}
+
+class  ReverseANumberUsingforLoop{
+    public static void main(String[] args)
+    {
+        int reverse = 0 ;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int Number = sc.nextInt();
+        sc.close();
+
+        for( ;Number != 0;
+        Number=Number/10)
+        {
+            int remainder = Number % 10;
+            reverse = reverse * 10 + remainder;
+        }
+        System.out.println("The reverse of the given number is: " + reverse);
+    }
+}
+
+class FibonacciSeries {
+     private static int n1=0,n2=1,n3=0;
+    static void printFibonacci(int count){
+        if(count>0){
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+            System.out.print(" "+n3);
+            printFibonacci(count-1);
+        }
+    }
+    public static void main(String args[]){
+        System.out.println("Enter the value of Count : ");
+        Scanner sc = new Scanner(System.in);
+         int count = sc.nextInt();
+        sc.close();
+
+        System.out.print(n1+" "+n2);
+        printFibonacci(count-2);
+    }
+}
